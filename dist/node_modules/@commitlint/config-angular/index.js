3825c5efa71b04c13dab01dbdb742d6ccba17823
Ahmad Nassri <ahmad@ahmadnassri.com>
2022-09-01 16:35:32 +0000

    fix: include node_modules in dist

---
+++ dist/node_modules/@commitlint/config-angular/index.js
@@ -0,0 +1,22 @@
+const typeEnum = require('@commitlint/config-angular-type-enum');
+
+module.exports = {
+	parserPreset: {parserOpts: {headerPattern: /^(\w*)(?:\((.*)\))?!?: (.*)$/}},
+	rules: {
+		'subject-exclamation-mark': [2, 'never'],
+		'body-leading-blank': [1, 'always'],
+		'footer-leading-blank': [1, 'always'],
+		'header-max-length': [2, 'always', 72],
+		'scope-case': [2, 'always', 'lower-case'],
+		'subject-case': [
+			2,
+			'never',
+			['sentence-case', 'start-case', 'pascal-case', 'upper-case'],
+		],
+		'subject-empty': [2, 'never'],
+		'subject-full-stop': [2, 'never', '.'],
+		'type-case': [2, 'always', 'lower-case'],
+		'type-empty': [2, 'never'],
+		'type-enum': typeEnum.rules['type-enum'],
+	},
+};
